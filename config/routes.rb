9516bf8033b73e522eb9018b72a274f02404ca28
Roopa Prakash <roopa.p.rao@gmail.com>
2017-08-19 16:20:57 -0700

    First Assignment

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
 
-  get 'contact' => 'pages#contact'
+  get 'contactus' => 'pages#contactus'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
