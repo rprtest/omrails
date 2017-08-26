e99fa4d329632e96c7019acd4476aa36af3a9796
Roopa Prakash <roopa.p.rao@gmail.com>
2017-08-26 10:57:23 -0700

    Import Bootstrap Template

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
 
-  get 'contactus' => 'pages#contactus'
+  get 'contact' => 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
